8a6ab16d16ec5a8167703345dccf44762c244da9
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-04 13:45:15 +0100

    question 1 solved

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,16 @@
 class AnagramHunter
 
   def initialize(word_list)
+    @word_list = ["try","how","eat","far","act","see","hit","lie","key","tac","sit","its","tea"]
   end
 
   def call(word)
+    word = alphabet(word)
+    @word_list.select { |w| alphabet(w) == word }
+  end
+
+  def alphabet(word)
+    word.chars.sort.join
   end
 
 end
